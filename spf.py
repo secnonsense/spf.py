5fbab899bc62e72624911cac9f9d3ec4da7c401a
secnonsense <scott@securitynonsense.com>
2017-04-18 20:57:38 -0400

    Add files via upload
    Anyone who has worked with anti-spam products knows the pain of dealing with anti-spoofing policies and running dig TXT commands through layers and layers of includes in SPF records.  This simple script automates that process.  Only ip4 addresses, includes and a records associated with mx records are processed at this time.

---
+++ spf.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+#uses dnspython - http://www.dnspython.org/
+import sys, re, dns.resolver
+
+x = y = 0
+spf = []
+lspf = []
+lookup = []
+mxlookup = []
+
+#First Lookup of Domain Passed as Argument - Added to lookup list 
+lookup.append(sys.argv[1])
+
+#Primary Loop for processing initial DNS lookup and itterating through includes
+while lookup:
+#Choose Record type of lookup - A for MX Records and TXT for SPF
+    if lookup[y] in mxlookup:
+        Q = "A"
+    else:
+        Q = "TXT"
+#DNS query and print domain name being processed
+    answers = dns.resolver.query(lookup[y], Q)
+    print "\ndomain block:  " + lookup[y] + "\n"
+
+    for server in answers:
+        lspf = re.split('\s|:', str(server))
+        spf = [s.strip('"') for s in lspf]
+        print '\n' + Q + ' Record: ' + ' '.join(spf) + '\n'
+#Loop to look at all elements of the returned record        
+        for i in range(0, len(spf)):
+            if spf[0] != 'v=spf1' and Q != "A":
+                print "No spf\n"
+#Add includes to lookup for further processing and increment counter
+            elif spf[i] == "include":
+                lookup.append(spf[i+1])
+                x = x+1
+#Add MX "A" records to lookup and mxlookup so their query can be handled properly                
+            elif spf[i] == "a":
+                lookup.append(spf[i+1])
+                mxlookup.append(spf[i+1])
+                x = x+1
+#Print the IPv4 addresses in an easily copiable block                
+            elif spf[i] == "ip4":
+                print spf[i+1]
+            elif Q == "A":
+                print spf[i]
+#Counter to be sure we don't loop beyond the end of the lookup list
+    y = y+1
+    if y > x:
+        quit()
