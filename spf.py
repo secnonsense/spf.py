3251bb8c6bd980b8a6c177799a8de9a8808efdb6
swinters <swinters@MacBook-Air.local>
2017-04-18 22:06:25 -0400

    first pass at ip6 fix

--- spf.py
+++ spf.py
@@ -10,7 +10,7 @@ lookup = []
 mxlookup = []
 
 #First Lookup of Domain Passed as Argument - Added to lookup list
-lookup.append(sys.argv[1])
+lookup.append("mailgun.com")
 
 #Primary Loop for processing initial DNS lookup and itterating through includes
 while lookup:
@@ -24,7 +24,7 @@ while lookup:
     print "\ndomain block:  " + lookup[y] + "\n"
 
     for server in answers:
-        lspf = re.split('\s|:', str(server))
+        lspf = re.split('\s', str(server))
         spf = [s.strip('"') for s in lspf]
         print '\n' + Q + ' Record: ' + ' '.join(spf) + '\n'
 #Loop to look at all elements of the returned record
@@ -32,8 +32,9 @@ while lookup:
             if spf[0] != 'v=spf1' and Q != "A":
                 print "No spf\n"
 #Add includes to lookup for further processing and increment counter
-            elif spf[i] == "include":
-                lookup.append(spf[i+1])
+            elif "include" in spf[i]:
+                inc = spf[i].split(":")
+                lookup.append(inc[1])
                 x = x+1
 #Add MX "A" records to lookup and mxlookup so their query can be handled properly
             elif spf[i] == "a":
@@ -41,8 +42,12 @@ while lookup:
                 mxlookup.append(spf[i+1])
                 x = x+1
 #Print the IPv4 addresses in an easily copiable block
-            elif spf[i] == "ip4":
-                print spf[i+1]
+            elif "ip4:" in spf[i]:
+                ip = spf[i].split(":")
+                print ip[1]
+            elif "ip6:" in spf[i]:
+                ip6 = spf[i]
+                print ip6[4:]
             elif Q == "A":
                 print spf[i]
 #Counter to be sure we don't loop beyond the end of the lookup list
