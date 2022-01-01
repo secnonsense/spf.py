2fef929892270ccf615701cb02910830be959a87
secnonsense <secnonsense>
2022-01-01 16:41:45 -0500

    Added docstrings

--- spf.py
+++ spf.py
@@ -1,82 +1,86 @@
 #!/usr/bin/python
+"""This script uses dnspython - http://www.dnspython.org/ to lookup spf records
+and iterate through all of the answers outputing IP addresses"""
 
-#uses dnspython - http://www.dnspython.org/
-import sys, re, dns.resolver
+import sys
+import re
+import dns.resolver
 
 def lookup_loop(lookup):
-    x = y = 0
+    """Main loop to do DNS lookups and parse out IP addresses"""
+    counter = counter2 = 0
     spf, lspf, alookup, mxlookup = ([] for z in range(4))
     #Primary Loop for processing initial DNS lookup and itterating through includes
     while lookup:
     #Choose Record type of lookup
-        if lookup[y] in alookup:
-            Q = "A"
-        elif lookup[y] in mxlookup:
-            Q = "MX"
+        if lookup[counter2] in alookup:
+            query_type = "A"
+        elif lookup[counter2] in mxlookup:
+            query_type = "MX"
         else:
-            Q = "TXT"
+            query_type = "TXT"
     #DNS query and print domain name being processed while handling any error exceptions
         try:
-            answers = dns.resolver.resolve(lookup[y], Q)
+            answers = dns.resolver.resolve(lookup[counter2], query_type)
         except dns.resolver.NXDOMAIN:
-            print("\nNo such domain %s" % lookup[y] + "\n")
+            print("\nNo such domain %s" % lookup[counter2] + "\n")
             quit()
         except dns.resolver.NoNameservers:
-            print("\nNo answers for domain %s" % lookup[y] + " for " + Q + " record query\n")
+            print("\nNo answers for domain %s" % lookup[counter2] + " for " + query_type + " record query\n")
             quit()
         except dns.resolver.NoAnswer:
-            print("\nNo " + Q + " record answer for domain %s" % lookup[y] + "\n")
+            print("\nNo " + query_type + " record answer for domain %s" % lookup[counter2] + "\n")
             quit()
-        print("\ndomain block:  " + lookup[y])
+        print("\ndomain block:  " + lookup[counter2])
 
         for server in answers:
             lspf = re.split('\s', str(server))
             spf = [s.strip('"') for s in lspf]
-            print('\n' + Q + ' Record: ' + ' '.join(spf) + '\n')
+            print('\n' + query_type + ' Record: ' + ' '.join(spf) + '\n')
     #Loop to look at all elements of the returned record
             for i in range(0, len(spf)):
-                if spf[0] != 'v=spf1' and Q != "A" and Q != "MX":
+                if spf[0] != 'v=spf1' and query_type != "A" and query_type != "MX":
                     print("No spf\n")
     #Add includes to lookup for further processing and increment counter
                 elif "include:" in spf[i]:
                     inc = spf[i].split(":")
                     lookup.append(inc[1])
-                    x = x+1
+                    counter += 1
     #Add a "A" records to lookup and alookup so their query can be handled properly
                 elif "a:" in spf[i].lower() and "ip6" not in spf[i].lower():
-                    a = spf[i].split(":")
-                    lookup.append(a[1])
-                    alookup.append(a[1])
-                    x = x+1
+                    split_value = spf[i].split(":")
+                    lookup.append(split_value[1])
+                    alookup.append(split_value[1])
+                    counter += 1
                 elif spf[i].lower() == "mx" or spf[i].lower() == "+mx":
-                    lookup.append(lookup[y])
-                    mxlookup.append(lookup[y])
-                    x = x+1
+                    lookup.append(lookup[counter2])
+                    mxlookup.append(lookup[counter2])
+                    counter += 1
                 elif spf[i].lower() == "a" or spf[i].lower() == "+a":
-                    lookup.append(lookup[y])
-                    alookup.append(lookup[y])
-                    x = x+1
+                    lookup.append(lookup[counter2])
+                    alookup.append(lookup[counter2])
+                    counter += 1
 
     #Print the IPv4 addresses in an easily copiable block
                 elif "ip4:" in spf[i].lower():
-                    ip = spf[i].split(":")
-                    if "/" not in ip[1]:
-                        print(ip[1] + "/32")
+                    ip_address = spf[i].split(":")
+                    if "/" not in ip_address[1]:
+                        print(ip_address[1] + "/32")
                     else:
-                        print(ip[1])
+                        print(ip_address[1])
                 elif "ip6:" in spf[i].lower():
                     ip6 = spf[i]
                     print(ip6[4:])
-                elif Q == "A":
+                elif query_type == "A":
                     print(spf[i] + "/32")
-                elif Q == "MX" and len(spf[i]) > 3:
+                elif query_type == "MX" and len(spf[i]) > 3:
                     lookup.append(spf[i])
                     alookup.append(spf[i])
-                    x = x+1
+                    counter += 1
     #Counter to be sure we don't loop beyond the end of the lookup list
-        y = y+1
+        counter2 += 1
         print("\n=====================================")
-        if y > x:
+        if counter2 > counter:
             print("\n")
             quit()
 
